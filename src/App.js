e9df00b7beecc1560d4a967d2002f235b3eec1c8
Angela Castagno <oesed16@gmail.com>
2019-02-26 00:08:13 -0500

    Reto 11 React: Administrar Invitados

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nombre: '',
+      apellido: '',
+      formData: []
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleApellidoChange = this.handleApellidoChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleNameChange(e) {
+    this.setState({
+      nombre: e.target.value
+    });
+  }
+
+  handleApellidoChange(e) {
+    this.setState({
+      apellido: e.target.value
+    });
+  }
+
+  handleFormSubmit(e) {
+    if (this.state.nombre !== "" && this.state.apellido !== "") {
+      this.setState({
+        formData: this.state.formData.concat({ nombre: this.state.nombre, apellido: this.state.apellido }),
+        nombre: '',
+        apellido: ''
+      });
+    }
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.handleFormSubmit} >
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" className="form-control" value={this.state.nombre} onChange={this.handleNameChange} name="first-name" />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" className="form-control" value={this.state.apellido} onChange={this.handleApellidoChange} name="last-name" />
               </div>
 
               <div className="action">
@@ -30,7 +65,12 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+              {this.state.formData.map((formData, index) => 
+              <tr key={index}>
+                <td>{formData.nombre}</td>
+                <td>{formData.apellido}</td>
+              </tr>
+              )}
               </tbody>
             </table>
           </div>
@@ -41,5 +81,3 @@ class App extends Component {
 }
 
 export default App
-
-
